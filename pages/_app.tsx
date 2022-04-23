aaee1d9a0fbb500f26fe841e7ee673f37d68a0ce
Elijah Cobb <elijah.cobb@vercel.com>
2022-04-23 13:27:35 -0400

    changed icon, title, date, sizing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/global.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>{"The Cobb's in NZ"}</title>
+      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
